1e2fccc7aa3406e75dbc0bf33b30b501c7fd877b
Ian <fleming22012@byui.edu>
2024-10-28 20:20:07 -0600

    Changes made for prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+        Console.WriteLine($"You're name is {last_name}, {first_name} {last_name}");
     }
 }
