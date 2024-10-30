5b816b8d5f45933364ddbb35abc8477992538ac3
Ian <fleming22012@byui.edu>
2024-10-30 17:10:58 -0600

    Core requirements for prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello user. What percentage did you get?");
+        string Grade = Console.ReadLine();
+
+        int number = int.Parse(Grade);
+
+        string letter = "";
+
+
+        if (number >=90)
+        {
+            //Console.WriteLine("You got an A");
+
+            letter = "A";
+
+        }
+        else if (number >=80)
+        {
+            //Console.WriteLine("You got a B.");
+            letter = "B";
+
+        }
+        else if (number >=70)
+        {
+            //Console.WriteLine("You got a C.");
+            letter = "C";
+
+        }
+        else if (number >=60)
+        {
+            //Console.WriteLine("You got a D.");
+            letter = "D";
+
+        }
+        else if (number <60)
+        {
+            //Console.WriteLine("You got an F.");
+            letter = "F";
+
+        }
+            if (number >=70)
+            {
+                Console.WriteLine("You passed the class! Congratulations!");
+            }
+            else
+            {
+                Console.WriteLine("You did not pass the class. Please see the teacher for a retake.");
+            }
+            Console.WriteLine($"You got a {letter}");
+
+
+
+    
     }
 }
