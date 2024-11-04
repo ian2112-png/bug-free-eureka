250416105b3ed96abfd903d6c671a12e167f5c24
Ian <fleming22012@byui.edu>
2024-11-04 11:05:45 -0700

    finished guessing number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,25);
+        
+        Console.WriteLine("Hello user. Guess the magic number ");
+        int userNumber;
+        do //do while loop guaranteed to run at least once
+        {
+            Console.WriteLine("What is your guess?");
+            string guess = Console.ReadLine();
+            userNumber = int.Parse(guess);
+            
+            
+            if (userNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (userNumber < magicNumber)
+            {
+                Console.WriteLine("Higher!"); 
+            }
+            else if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+
+            }
+        }while (userNumber != magicNumber);
+
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
-class Program
+class Program //creating a class
 {
-    static void Main(string[] args)
+    static int AddNumbers (int n1, int n2)  //AddNumbers is a method
     {
-        Console.WriteLine("Hello World!!!");
+        int total = (n1 + n2);
+        return total;
+    }
+    static void Main(string[] args) //Main is a method
+    {
+        int total = AddNumbers(20, 30);
+        Console.WriteLine(total);
+    
+        Console.WriteLine("Hello bob");
+        
+        for (int i = 0 ; i < 10; i++) // declares i=0 first, then checks if 0<10, then prints line, then increments. loop continues
+        {
+            Console.WriteLine(i);
+        }
+        int age = -1;
+        while (age < 0 || age > 120) //while loop will continue as long as the expression is true
+        {
+            Console.Write("Please enter your age: (0-125) ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Your age is: {age}");
+        }
+        List <string> myColors = new List<string>();  //before = says I am creating list of type string. after = creates new list 
+        myColors.Add("Red");
+        myColors.Add("Green");
+        myColors.Add("Blue");
+
+        foreach (string color in myColors)
+        {
+            Console.WriteLine(color);
+        }
     }
 }
