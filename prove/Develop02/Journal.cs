31f9f8951341699030476ed019114790cd6ac15d
Ian <fleming22012@byui.edu>
2024-11-13 09:57:50 -0700

    First iteration of journal program.

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+public class Journal
+{
+   private List<Entry> _entries = new List<Entry>();
+   private int _wordCount;
+
+
+    // public Journal()
+    // {
+    //     //_entries = new List<Entry>();
+    // }
+   public void DisplayEntries()
+   {
+    Console.WriteLine("Hello bob");
+   }
+}
+
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the journal program!");
+        Journal journal = new Journal();
+        journal.DisplayEntries();
     }
 }
+//For split in order to index, put a command to user to put a $ at the end of their journal entry.
+//This way I can access it with an index during the split. 
+// Journal nouns: 
+// -entry
+//  -prompts
+// -journal
+//-menu
+//-program 
