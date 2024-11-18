c30e794f26fc1b6e438cab57a8e7b7f03ceda175
Ian <fleming22012@byui.edu>
2024-11-18 16:44:33 -0700

    Added fraction getters and setters and get fraction string.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
-    }
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        // Console.WriteLine(fraction1);
+
+        // Fraction fraction2 = new Fraction(4);
+        // Console.WriteLine(fraction2);
+
+        // Fraction fraction3 = new Fraction(3,4);    
+        // Console.WriteLine(fraction3);
+        
+        }
 }
