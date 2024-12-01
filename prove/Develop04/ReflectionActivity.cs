2c7fad497741f2934c30c40cfc921195c638af50
Ian <fleming22012@byui.edu>
2024-11-30 19:29:05 -0700

    I added prompts and questions to the list within the reflection activity constructor.

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -7,6 +7,22 @@ class ReflectionActivity : Activity
    public ReflectionActivity(string specificDescription)  
    {
     _specificDescription = specificDescription;
+    
+    _prompts.Add("Think of a time when you stood up for someone else.");
+    _prompts.Add("Think of a time when you did something really difficult.");
+    _prompts.Add("Think of a time when you helped someone in need.");
+    _prompts.Add("Think of a time when you did something truly selfless.");
+
+    _reflectionQuestions.Add("Why was this experience meaningful to you?");
+    _reflectionQuestions.Add("Have you ever done anything like this before?");
+    _reflectionQuestions.Add("How did you get started?");
+    _reflectionQuestions.Add("How did you feel when it was complete?");
+    _reflectionQuestions.Add("What made this time different than other times when you were not as successful?");
+    _reflectionQuestions.Add("What is your favorite thing about this experience?");
+    _reflectionQuestions.Add("What could you learn from this experience that applies to other situations?");
+    _reflectionQuestions.Add("What did you learn about yourself through this experience?");
+    _reflectionQuestions.Add("How can you keep this experience in mind in the future?");
+    
    }
    public override string ToString()
     {
@@ -31,4 +47,5 @@ class ReflectionActivity : Activity
     Random random2 = new Random();
     int index2 = random2.Next(_reflectionQuestions.Count);
     return _reflectionQuestions[index2];
+   }
 }
