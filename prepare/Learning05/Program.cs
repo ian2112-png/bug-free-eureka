9c0cd0678594ddc37db3ac5e3d187a7f8ddf4376
Ian <fleming22012@byui.edu>
2024-12-03 18:45:17 -0700

    I added all classes and methods.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Shape square = new Square("blue",12);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+    
+    Shape circle = new Circle("green",5);
+    Console.WriteLine(circle.GetColor());
+    Console.WriteLine(circle.GetArea());
+
+    Shape rectangle = new Rectangle("red",5, 4);
+    Console.WriteLine(rectangle.GetColor());
+    Console.WriteLine(rectangle.GetArea());
     }
+        
+
 }
